2445a1a7f4f118e4c04fc3e508044cb04b350cb2
Seth Lachman <lachmanseth17@gmail.com>
2019-04-09 16:06:31 -0400

    Added scroll event handler.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,21 @@
 
 const logoHeading = document.querySelector('.logo-heading');
 
+// mouseover event listener 1/10
 logoHeading.addEventListener('mouseover', growHeader);
 
 function growHeader(event) {
   event.target.classList.add('hover');
 }
 
+// mouseleave event listener 2/10
 logoHeading.addEventListener('mouseleave', shrinkHeader);
 
 function shrinkHeader(event) {
   event.target.classList.remove('hover');
 }
 
+// dblclick event listener 3/10
 logoHeading.addEventListener('dblclick', notifyUser);
 
 const notificationDiv = document.createElement('div');
@@ -22,3 +25,18 @@ notificationDiv.className = "notification";
 function notifyUser(event) {
   event.target.appendChild(notificationDiv);
 }
+
+
+// Scroll Event 4/10
+const mainNavigation = document.querySelector('.main-navigation');
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > 20) {
+    mainNavigation.style.opacity = 0.5;
+  } else {
+    mainNavigation.style.opacity = 1;
+  }
+});
+
+
+
+
