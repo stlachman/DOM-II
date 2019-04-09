e245b37c9fa19238cdad98184bef8bad6e4fd2d1
Seth Lachman <lachmanseth17@gmail.com>
2019-04-09 17:44:57 -0400

    Added event handler for drag.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
-
 const logoHeading = document.querySelector('.logo-heading');
 
 // mouseover event listener 1/10
 logoHeading.addEventListener('mouseover', growHeader);
 
 function growHeader(event) {
-  event.target.classList.add('hover');
+	event.target.classList.add('hover');
 }
 
 // mouseleave event listener 2/10
 logoHeading.addEventListener('mouseleave', shrinkHeader);
 
 function shrinkHeader(event) {
-  event.target.classList.remove('hover');
+	event.target.classList.remove('hover');
 }
 
 // dblclick event listener 3/10
@@ -20,49 +19,49 @@ logoHeading.addEventListener('dblclick', notifyUser);
 
 const notificationDiv = document.createElement('div');
 notificationDiv.innerHTML = "<p>I don't go anywhere!</p>";
-notificationDiv.className = "notification";
+notificationDiv.className = 'notification';
 
 function notifyUser(event) {
-  event.target.appendChild(notificationDiv);
+	event.target.appendChild(notificationDiv);
 }
-
 
 // Scroll Event 4/10
 const mainNavigation = document.querySelector('.main-navigation');
 window.addEventListener('scroll', function() {
-  if (window.pageYOffset > 20) {
-    mainNavigation.style.opacity = 0.5;
-  } else {
-    mainNavigation.style.opacity = 1;
-  }
+	if (window.pageYOffset > 20) {
+		mainNavigation.style.opacity = 0.5;
+	} else {
+		mainNavigation.style.opacity = 1;
+	}
 });
-
 
 // Resize 5/10
 window.addEventListener('resize', function(event) {
-  if (event.target.outerWidth < 1000) {
-    mainNavigation.style.padding = "0 20px";
-  } else {
-    mainNavigation.style.padding = "0";
-  }
+	if (event.target.outerWidth < 1000) {
+		mainNavigation.style.padding = '0 20px';
+	} else {
+		mainNavigation.style.padding = '0';
+	}
 });
 
 // Focus 6/10
 
 const nav = document.querySelector('.nav');
 
-nav.addEventListener('focus', (event) => {
-  event.target.style.background = '#bada55';
-  event.target.style.padding = '10px'
-}, true); 
-
+nav.addEventListener(
+	'focus',
+	(event) => {
+		event.target.style.background = '#bada55';
+		event.target.style.padding = '10px';
+	},
+	true
+);
 
 // Load 7/10
 
 window.addEventListener('load', (event) => {
-  document.querySelector('body').classList.add('loaded');
+	document.querySelector('body').classList.add('loaded');
 });
-
 
 // Click 8/10
 const contentBtns = document.querySelector('.content-pick');
@@ -70,20 +69,41 @@ const contentBtns = document.querySelector('.content-pick');
 contentBtns.addEventListener('click', nudge);
 
 function nudge(event) {
-  if (Array.from(event.target.classList).includes("btn")) {
-    event.target.classList.toggle("nudge");
-  }
+	if (Array.from(event.target.classList).includes('btn')) {
+		event.target.classList.toggle('nudge');
+	}
 }
 
 // keydown 9/10
-let egg = "";
+let egg = '';
 
 window.addEventListener('keydown', easterEgg);
 
 function easterEgg(event) {
-  egg = egg + event.key;
-  if (egg === "funbus") {
-    alert("Fun BUS FTW!")
-    window.removeEventListener("keydown", easterEgg);
-  }
+	egg = egg + event.key;
+	if (egg === 'funbus') {
+		alert('Fun BUS FTW!');
+		window.removeEventListener('keydown', easterEgg);
+	} else if (egg.length > 6) {
+		window.removeEventListener('keydown', easterEgg);
+	}
 }
+
+// drag 10/10
+
+const funBusImg = document.querySelector('.intro img');
+
+let dragged;
+
+funBusImg.addEventListener('drag', (event) => {});
+
+funBusImg.addEventListener('dragstart', (event) => {
+	dragged = event.target;
+
+	event.target.style.border = '2px solid red';
+});
+
+funBusImg.addEventListener('dragend', (event) => {
+	// reset the transparency
+	event.target.style.border = '';
+});
