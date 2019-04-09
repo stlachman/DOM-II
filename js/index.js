cb69a4a00ef2d270f4c8037a7c4af37ea2e189ee
Seth Lachman <lachmanseth17@gmail.com>
2019-04-09 15:18:16 -0400

    Added first event listeners to fun bus website and updated less styles for transition effects.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+
+const logoHeading = document.querySelector('.logo-heading');
+
+logoHeading.addEventListener('mouseover', growHeader);
+
+function growHeader(event) {
+  event.target.classList.add('hover');
+}
+
+logoHeading.addEventListener('mouseleave', shrinkHeader);
+
+function shrinkHeader(event) {
+  event.target.classList.remove('hover');
+}
